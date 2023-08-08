0bee2e323542fe289accc1a97952f59ed704c718
Rostislav Rusakov <134939763+rostrskv@users.noreply.github.com>
2023-08-08 11:13:24 +0300

    Add score color

--- src/components/Score.js
+++ src/components/Score.js
@@ -3,6 +3,10 @@
  * @param {Object} params 
  * @param {number} params.score
  */
-export default function Score({score}){
-    return <div>{score}</div>
+export default function Score({ score }) {
+    return <div className={
+        score >= 80 ? "high-score"
+            : score >= 50 ? "medium-score"
+                : "low-score"
+    }>{score}</div>
 }
